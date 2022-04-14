d21fb38aeedbb29f0890e79a2f29e4b32602b1b2
annakilchytska <annushka.kilchytska@gmail.com>
2022-04-14 21:50:51 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1c397815-3830-4979-a826-262571a9bff5';
+export const MODAL_CLOSE_SEC = 2.5;
